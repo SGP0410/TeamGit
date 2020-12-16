b7bf08bc0352123b142ce733f93f533895ef0726
SGP0410 <2742837998@qq.com>
2020-12-17 00:37:20 +0800

    第3次

--- app/src/main/java/com/example/teamgit/MainActivity.java
+++ app/src/main/java/com/example/teamgit/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "我是关鑫", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我是李吉达", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "成守宇", Toast.LENGTH_SHORT).show();
     }
 }
