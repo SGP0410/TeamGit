4fabd10499e8366721a73387f1ac82a21c60363a
XGKerwin <1064936282@qq.com>
2020-12-17 13:33:19 +0800

    X_第4次Ssss001

---
+++ app/src/main/java/com/example/teamgit/SSXXXXX.java
@@ -0,0 +1,15 @@
+package com.example.teamgit;
+
+/**
+ * author : 关鑫
+ * Github : XGKerwin
+ * date   : 2020/12/17 13:32
+ */
+public class SSXXXXX {
+
+    private static final String TAG = "SSXXXXX";
+
+    //z这是我的写的谁都别改
+
+
+}
