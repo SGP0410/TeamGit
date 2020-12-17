1d20c7f34c6b3e9bcb2d190811a937ad0708e7a5
XGKerwin <1064936282@qq.com>
2020-12-17 17:30:14 +0800

    X_第4次

---
+++ app/src/main/java/com/example/teamgit/Activity_the1.java
@@ -0,0 +1,20 @@
+package com.example.teamgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class Activity_the1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the1);
+
+        Log.i("vvvvvv", "onCreate: ");
+
+
+    }
+}
---
+++ app/src/main/java/com/example/teamgit/X_th2Activity.java
@@ -0,0 +1,17 @@
+package com.example.teamgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class X_th2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_x_th2);
+
+
+
+    }
+}
