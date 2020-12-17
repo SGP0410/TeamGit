2ea0211ecfaca3a03c53a9aac8d89529a4299211
XGKerwin <1064936282@qq.com>
2020-12-17 17:47:07 +0800

    X_第4次Ssssss

--- app/src/main/java/com/example/teamgit/MainActivity3.java
+++ app/src/main/java/com/example/teamgit/MainActivity3.java
@@ -2,10 +2,14 @@ package com.example.teamgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private String TAG = "MainActivity3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,11 @@ public class MainActivity3 extends AppCompatActivity {
         Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+
+
+        Intent intent = new Intent(MainActivity3.this,MainActivity.class);
+        startActivity(intent);
+        Log.i(TAG, "onCreate: ");
 
 
     }
