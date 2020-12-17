3f16b6184ec3628c346bf86b499fbdac3db0aba3
XGKerwin <1064936282@qq.com>
2020-12-17 17:40:10 +0800

    X_第4次Sssss

---
+++ app/src/main/java/com/example/teamgit/MainActivity14.java
@@ -0,0 +1,21 @@
+package com.example.teamgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity14 extends AppCompatActivity {
+
+    private String TAG = "main_143";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main14);
+
+        Log.i(TAG, "onCreate: ");
+
+
+    }
+}
