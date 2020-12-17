3c6bc397a4ef8df0a88aea9fffdf674ee2bc2dd3
SGP0410 <2742837998@qq.com>
2020-12-17 17:39:53 +0800

    第7次

---
+++ app/src/main/java/com/example/teamgit/MainActivity3.java
@@ -0,0 +1,28 @@
+package com.example.teamgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
